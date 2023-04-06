2b6cd657da5194c8ffc80263a1449e928bb49373
andmohiko <andmohiko@gmail.com>
2023-04-07 01:31:28 +0900

    feat: loading animation

---
+++ src/components/Base/LoadingAnimation.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export const LoadingAnimation = (): React.ReactElement => {
+  return (
+    <Image src="/images/svgs/tail-spin.svg" height="80" width="80" alt="" />
+  )
+}
