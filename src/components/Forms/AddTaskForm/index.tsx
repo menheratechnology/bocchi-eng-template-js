53b9d693111fe905db73ddf06925b00819acccb6
okamonster <okamonster0615@gmail.com>
2023-11-15 23:22:36 +0900

    fix:ディレクトリ構成修正

--- src/components/Forms/AddTaskForm/index.tsx
+++ src/components/Forms/AddTaskForm/index.tsx
@@ -3,9 +3,9 @@ import { Dispatch, SetStateAction, useState } from 'react'
 import styles from './style.module.scss'
 
 // ↓これらのコンポーネントを使ってください
-import { BasicButton } from '~/Buttons/BasicButton'
-import { TextAreaInput } from '~/Inputs/TextAreaInput'
-import { TextInput } from '~/Inputs/TextInput'
+import { BasicButton } from '~/components/Buttons/BasicButton'
+import { TextAreaInput } from '~/components/Inputs/TextAreaInput'
+import { TextInput } from '~/components/Inputs/TextInput'
 import { Task } from '~/entities/Task'
 
 type Props = {
@@ -22,15 +22,37 @@ export const AddTaskForm = ({
 
   const addTask = () => {
     // TODO: タスクを追加する処理を書く
+    const newTask: Task = {
+      title: title,
+      description: description,
+      isComplete: false,
+    }
+
+    const newIncompleteTasks = [...incompleteTasks, newTask]
+
+    setIncompleteTasks(newIncompleteTasks)
   }
   return (
     <div className={styles.formWrapper}>
       <p>新しいタスクを追加</p>
       {/* TODO: タスク名を入力するinputを用意する */}
-
+      <TextInput
+        label="タスク名"
+        onChange={(e) => {
+          setTitle(e.target.value)
+        }}
+        value={title}
+      />
       {/* TODO: タスクの詳細を入力するtextareaを用意する */}
-
+      <TextAreaInput
+        label="詳細"
+        onChange={(e) => {
+          setDescription(e.target.value)
+        }}
+        value={description}
+      />
       {/* TODO: タスクを保存するボタンをつくる */}
+      <BasicButton onClick={addTask}>タスクを追加</BasicButton>
     </div>
   )
 }
